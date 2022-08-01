5cf3ec70c72f1e3ab0428fd05814d2df64d5ae98
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-08-01 15:05:43 -0500

    update database.sql

--- database.sql
+++ database.sql
@@ -6,5 +6,29 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "tarkov_name" VARCHAR (80) UNIQUE NOT NULL,
+    "tarkov_level" INT NOT NULL,
+    "access_level" INT DEFAULT 0
+);
+
+CREATE TABLE "messages" (
+    "id" SERIAL PRIMARY KEY,
+    "description" VARCHAR(300),
+    "time" VARCHAR(100),
+    "user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "private_messages" (
+    "id" SERIAL PRIMARY KEY,
+    "message" VARCHAR(300),
+    "time" VARCHAR(100),
+    "user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "user_private_messages" (
+    "id" SERIAL PRIMARY KEY,
+    "message_id" INT REFERENCES "private_messages" ON DELETE CASCADE,
+    "sender_user_id" INT REFERENCES "user",
+    "receiver_user_id" INT REFERENCES "user"
 );
