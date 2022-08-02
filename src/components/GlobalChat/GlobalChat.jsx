d590dad96a05b98d089b8b05d2aec54bd3281566
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-08-02 13:40:20 -0500

    change page layout for chat page

--- src/components/GlobalChat/GlobalChat.jsx
+++ src/components/GlobalChat/GlobalChat.jsx
@@ -2,6 +2,27 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import socket from '../../socket/socket';
+import './GlobalChat.css';
+
+//? Testing
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+//? end of those testing
 
 function GlobalChat() {
 	const [message, setMessage] = useState('');
@@ -33,25 +54,67 @@ function GlobalChat() {
 	}, [socket, dispatch]);
 
 	return (
-		<>
-			<input type='text' placeholder='Message' onChange={(e) => setMessage(e.target.value)} />
-			<button onClick={sendMessage}>SEND MESSAGE</button>
-
-			{messages.map((message) => (
-				<div key={message.id}>
-					{message.user_id === user.id ? (
-						<h2>
-							ME <span>{message.time}</span>
-						</h2>
-					) : (
-						<h2 onClick={() => privateMessage(message.user_id)}>
-							{message.username} <span>{message.time}</span>
-						</h2>
-					)}
-					<p>{message.description}</p>
-				</div>
-			))}
-		</>
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar
+				position='fixed'
+				sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+				<Toolbar>
+					<Typography variant='h6' noWrap component='div'>
+						Tarkov Connect
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				sx={{
+					width: drawerWidth,
+					flexShrink: 0,
+					'& .MuiDrawer-paper': {
+						width: drawerWidth,
+						boxSizing: 'border-box',
+					},
+				}}
+				variant='permanent'
+				anchor='left'>
+				<Toolbar />
+				USERNAME
+				<Divider />
+				<List>
+					{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+						<ListItem key={text} disablePadding>
+							<ListItemButton>
+								<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+								<ListItemText primary={text} />
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
+				<Divider />
+			</Drawer>
+			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+				<Toolbar />
+				<List style={{ maxHeight: 550, overflow: 'auto' }}>
+					{messages.map((message) => (
+						<div className='messageCard' key={message.id}>
+							{message.user_id === user.id ? (
+								<h2>
+									ME <span>{message.time}</span>
+								</h2>
+							) : (
+								<h2 onClick={() => privateMessage(message.user_id)}>
+									{message.username} <span>{message.time}</span>
+								</h2>
+							)}
+							<p>{message.description}</p>
+						</div>
+					))}
+				</List>
+				<center>
+					<input type='text' name='' id='' />
+					<button>Click Me</button>
+				</center>
+			</Box>
+		</Box>
 	);
 }
 
